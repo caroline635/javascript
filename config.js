09cb7bf8864d90f5545eb75776d6f9f2ca8dd60c
carol <80597333+caroline635@users.noreply.github.com>
2021-05-19 23:41:05 -0300

    adicionando o index.html
    adicionando o index.html no meu site

---
+++ config.js
@@ -0,0 +1,5 @@
+
+const nome = prompt ('Qual é o seu nome?');
+const ano =  prompt ('Qual ano você nasceu?');
+const idade = 2021 - ano;
+alert ('Olá seu nome é  ' +  nome + ' e você tem ' + idade  + ' anos ');
